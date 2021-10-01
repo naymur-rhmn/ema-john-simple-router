2d884c2cd27dd8b90fa1242678d9754f238bb765
naymur <naymurmn@gmail.com>
2021-10-01 18:40:00 +0600

    first try

---
+++ src/components/MyCompo/MyCompo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyCompo = () => {
+    return (
+        <div>
+            <h1>This is my experiment compo</h1>
+        </div>
+    );
+};
+
+export default MyCompo;
